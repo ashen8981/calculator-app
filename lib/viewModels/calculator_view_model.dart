83764627e7554f467d772f50be048e01313cbc40
Ashen Malindu <ashenmalindu112@gmail.com>
2025-05-10 21:15:30 +0530

    vm

--- lib/viewModels/calculator_view_model.dart
+++ lib/viewModels/calculator_view_model.dart
@@ -2,5 +2,43 @@ import 'package:flutter/material.dart';
 import '../models/calculator_model.dart';
 
 class CalculatorViewModel extends ChangeNotifier {
-  CalculatorModel _calculator = CalculatorModel(number1: 0.0, number2: 0.0);
+  final CalculatorModel _calculator = CalculatorModel(number1: 0.0, number2: 0.0);
+
+  double get number1 => _calculator.number1;
+  double get number2 => _calculator.number2;
+  double get result => _calculator.result;
+
+  void setNumber1(double value) {
+    _calculator.number1 = value;
+    notifyListeners();
+  }
+
+  void setNumber2(double value) {
+    _calculator.number2 = value;
+    notifyListeners();
+  }
+
+  void add() {
+    _calculator.result = _calculator.number1 + _calculator.number2;
+    notifyListeners();
+  }
+
+  void subtract() {
+    _calculator.result = _calculator.number1 - _calculator.number2;
+    notifyListeners();
+  }
+
+  void multiply() {
+    _calculator.result = _calculator.number1 * _calculator.number2;
+    notifyListeners();
+  }
+
+  void divide() {
+    if (_calculator.number2 != 0) {
+      _calculator.result = _calculator.number1 / _calculator.number2;
+    } else {
+      _calculator.result = double.nan;
+    }
+    notifyListeners();
+  }
 }
