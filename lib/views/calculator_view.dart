53d0f534858aae114c2df5b09d55e67361193e98
Ashen Malindu <ashenmalindu112@gmail.com>
2025-05-10 16:25:44 +0530

    ui

--- lib/views/calculator_view.dart
+++ lib/views/calculator_view.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
 
 class CalculatorView extends StatelessWidget {
-  const CalculatorView({super.key});
+  CalculatorView({super.key});
 
+  final TextEditingController number1Controller = TextEditingController();
+  final TextEditingController number2Controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('MVVM Calculator'), centerTitle: true),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: number1Controller,
+              decoration: const InputDecoration(labelText: 'Number 1'),
+              keyboardType: TextInputType.number,
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              controller: number2Controller,
+              decoration: const InputDecoration(labelText: 'Number 2'),
+              keyboardType: TextInputType.number,
+            ),
+            const SizedBox(height: 32),
+            Wrap(
+              spacing: 10,
+              children: [
+                ElevatedButton(onPressed: () {}, child: const Text('+')),
+                ElevatedButton(onPressed: () {}, child: const Text('-')),
+                ElevatedButton(onPressed: () {}, child: const Text('×')),
+                ElevatedButton(onPressed: () {}, child: const Text('÷')),
+              ],
+            ),
+            const SizedBox(height: 32),
+            Text('Result', style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+          ],
+        ),
+      ),
+    );
   }
 }
