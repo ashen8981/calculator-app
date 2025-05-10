aeb1cbf13f148c2c2d6da81f06a3838f93a88c6e
Ashen Malindu <ashenmalindu112@gmail.com>
2025-05-10 16:31:50 +0530

    vm

--- lib/viewModels/calculator_view_model.dart
+++ lib/viewModels/calculator_view_model.dart
@@ -1,1 +1,6 @@
+import 'package:flutter/material.dart';
+import '../models/calculator_model.dart';
 
+class CalculatorViewModel extends ChangeNotifier {
+  CalculatorModel _calculator = CalculatorModel(number1: 0.0, number2: 0.0);
+}
