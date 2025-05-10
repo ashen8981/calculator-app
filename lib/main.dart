5587adaf9ab530031e9b197f15c7f5a30fb77fe4
Ashen Malindu <ashenmalindu112@gmail.com>
2025-05-10 21:14:51 +0530

    cc

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,7 @@ import 'viewmodels/calculator_view_model.dart';
 import 'views/calculator_view.dart';
 
 void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (_) => CalculatorViewModel(),
-      child: const MyApp(),
-    ),
-  );
+  runApp(ChangeNotifierProvider(create: (_) => CalculatorViewModel(), child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -17,10 +12,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'MVVM Calculator',
-      home: CalculatorView(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, title: 'MVVM Calculator', home: CalculatorView());
   }
 }
